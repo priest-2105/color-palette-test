d51cdbb59013c86ff9de8167e40e944201baf312
priest-2105 <fawzybailey782@gmail.com>
2024-08-23 03:22:48 +0100

    script ✨✨

--- super.js
+++ super.js
@@ -1,14 +1,27 @@
 const questionColor =  document.getElementById('questionColor');
 const answerColor =  document.getElementById('answerColor');
 const testButton = document.getElementById('testButton');
-
+const closeResultButton = document.getElementById('closeResultModalButton');
 
 
 testButton.addEventListener('click', () => {
 
-console.log('listening');
+
+   const quesitonColorValue = questionColor.value
+   const randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+   questionColor.value = randomColor;
+
+   console.log(quesitonColorValue);
+    
+    
+
+});
 
 
 
 
+
+closeResultButton.addEventListener('click', () => {
+  const Result = document.getElementById('resultModal');
+  Result.style.display = 'none';
 });
